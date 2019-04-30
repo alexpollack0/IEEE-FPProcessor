818fa42b7cbb02f75570725d3181f9c9c487b636
Alex Pollack <azpollack97@gmail.com>
2019-04-30 14:53:31 -0400

    All steps completed aside from 5, 7, DONE

--- FinalProject.srcs/sources_1/new/FPAdder.v
+++ FinalProject.srcs/sources_1/new/FPAdder.v
@@ -25,6 +25,7 @@ module FPAdder(reset, clk, a, b, op, res);
     input reset, clk;
     input op; // 0 = addition, 1 = subtraction
     input [31:0] a, b;
+    output outSign;
     output [31:0] res;
 
     reg [2:0] state, next_state;
@@ -32,7 +33,8 @@ module FPAdder(reset, clk, a, b, op, res);
     // Values to extract in INIT
     reg aSign, bSign;
     reg [7:0] aExp, bExp;
-    reg [22:0] aFract, bFract, bFractComp;
+    reg [22:0] aFract, bFract;
+    reg [31:0] bFractComp;
 
     reg tempSign;
     reg [7:0] tempExp;
@@ -42,18 +44,28 @@ module FPAdder(reset, clk, a, b, op, res);
     reg setComp;
     reg [22:0] shiftRegB; // TODO: This needs to be a p-bit register, is 23 bits correct?
     reg [22:0] shiftedOut; // TODO: Determine width
+    reg [31:0] regSTemp;
     reg [22:0] regS;
+    reg [22:0] regSTwosComp;
     reg [22:0] regFiveS;
-    reg cOut;
+    reg fiveCOut;
 
     reg d, totalShift; // TODO: Determine width
     reg guard, round, sticky;
     reg fiveLeft;
 
+    reg [3:0] eightCase;
+
     localparam ADD = 1'b0, SUB = 1'b1;
 
     localparam INIT = 3'b000, SWAP_COMP = 3'b001, SHIFTB = 3'b010, SHIFT_DIRECT = 3'b011,
-                ADD_SHIFT = 3'b100, ADJUST =3'b101, ROUND_COMP = 3'b110;
+                ADD_SHIFT = 3'b100, ADJUST =3'b101, ROUND_COMP = 3'b110, DONE = 3'b111;
+
+    TwosComplement #(.SIZE(5))twoB(.in({9'b0, bFract}), .out(bFractComp));
+
+    VariableCLA #(.SIZE(5)) fourS(.a({9'b0, aFract}), .b({9'b0, aFract}), .c_in(1'b0), .s(regSTemp), .c_out(fiveCOut));
+
+    TwosComplement #(.SIZE(5))twoB(.in({9'b0, regS}), .out({9'b0, regSTwosComp}));
 
     always @(clk) begin
 
@@ -96,8 +108,11 @@ module FPAdder(reset, clk, a, b, op, res);
 
               end
 
+              // Tentatively set the exponent of the result to e1
+              outSign = aExp;
+
               if(aSign != bSign) begin
-                bFract = bFractComp;
+                bFract = bFractComp[22:0];
                 setComp = 1;
               end
 
@@ -106,9 +121,16 @@ module FPAdder(reset, clk, a, b, op, res);
 
             // Step 3
             SHIFTB: begin
-              d = aExp - bExp; // TODO: Implement this in a proper manner (with a subtractor)
-              totalShift = d;
+
+              // Place b mantissa in a p-bit register
               shiftRegB = bFract;
+
+              // Calculate d (how many bits to shift shiftRegB)
+              d = aExp - bExp; // TODO: Implement this in a proper manner (with a subtractor)
+
+              // Important to store for later
+              totalShift = d;
+
               if(d > 0) begin
                 next_state = SHIFT_DIRECT;
               end
@@ -121,11 +143,13 @@ module FPAdder(reset, clk, a, b, op, res);
             // Step 3 (continued)
             SHIFT_DIRECT: begin
 
-              shiftedOut = {shiftedOut[21:0], shiftRegB[0]};
+              shiftedOut = {shiftRegB[0], shiftedOut[21:0]};
 
+              // Shift in 1's if b fraction was completed in Step 2
               if(setComp) begin
                 shiftRegB = {1'b1, shiftRegB[22:1]};
               end
+              // Otherwise, shift in 0's
               else begin
                 shiftRegB = {1'b0, shiftRegB[22:1]};
               end
@@ -145,12 +169,13 @@ module FPAdder(reset, clk, a, b, op, res);
 
               // Finish initialization from Step 3
               sticky = 0;
+
               integer i;
-              for(i = 0; i < totalShift; i = i + 1) begin // TODO: Can we use 'for' loops (curious because of addition)
-                if(i == (totalShift - 1)) begin
+              for(i = 22; i > totalShift; i = i - 1) begin
+                if(i == 22) begin
                   guard = shiftedOut[i];
                 end
-                else if(i == (totalShift - 2)) begin
+                else if(i == 21) begin
                   round = shiftedOut[i];
                 end
                 else begin
@@ -161,7 +186,9 @@ module FPAdder(reset, clk, a, b, op, res);
               end
 
               // Step 4
-              // TODO: Add aFract to shiftRegB (call output reg regS)
+
+              // Add aFract to shiftRegB (call output register regS)
+              regS = regSTemp[22:0];
 
               // This can only possibly be hit when totalShift = 0
               if(aSign != bSign && regS[22] && !cOut) begin
@@ -170,11 +197,16 @@ module FPAdder(reset, clk, a, b, op, res);
 
               // Step 5
               if(aSign == bSign && !cOut) begin
-                regFiveS = {1'b1, S[21:0]};
+                regFiveS = {1'b1, regS[22:1]};
                 fiveLeft = 0;
               end
-              // TODO: Shift S left until it is normalized
               else begin
+                // On first left shift, fill in low-order position with the guard bit
+                regFiveS = {S[21:0], guard};
+
+                // TODO: Shift S left until it is normalized
+                // Shifted value will be placed in regFiveS
+                // Number of bits shifted will be in fiveBitShift
 
                 fiveLeft = 1;
               end
@@ -187,7 +219,10 @@ module FPAdder(reset, clk, a, b, op, res);
             ADJUST: begin
 
               if(fiveLeft) begin
-
+                if(fiveBitShift > 0) begin
+                  round = 0;
+                  shift = 0;
+                end
               end
               else begin
                 round = regS[0];
@@ -199,11 +234,30 @@ module FPAdder(reset, clk, a, b, op, res);
             // Steps 7, 8
             ROUND_COMPUTE: begin
 
-              if(aSign != bSign) begin
-                if(swapped) begin
+              // Step 7: Round to nearest even
 
-                end
+              // Step 8
+              if(aSign != bSign) begin
+                eightCase = {swapped, setComp, aSign, bSign};
+
+                case(eightCase)
+                  4'b1001: outSign = 1;
+                  4'b1101: outSign = 1;
+                  4'b0001: outSign = 0;
+                  4'b0010: outSign = 1;
+                  4'b0101: outSign = 1;
+                  4'b0110: outSign = 0;
+                  default: outSign = 0;
+                endcase
+
               end
+              else begin
+                outSign = aSign;
+              end
+
+            end
+
+            DONE: begin
 
             end
 
