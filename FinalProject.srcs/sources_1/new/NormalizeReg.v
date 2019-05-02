54a2cda160a015819a500a8afb7b780a4a8a2806
Alex Pollack <azpollack97@gmail.com>
2019-05-02 16:46:59 -0400

    Minor changes to Normalizer, Complement, and Variable CLA

--- FinalProject.srcs/sources_1/new/NormalizeReg.v
+++ FinalProject.srcs/sources_1/new/NormalizeReg.v
@@ -1,16 +1,19 @@
 `timescale 1ns / 1ps
 
-module NormalizeReg #(SIZE=1) (
-    input [2**SIZE:0] mantissa_in,
-    output [2**SIZE:0] mantissa_out,
-    output [SIZE-1:0] shiftNum
+module NormalizeReg (
+    input [2**SIZE-1:0] mantissa_in,
+    output [2**SIZE-1:0] mantissa_out,
+    output [SIZE:0] shiftNum
     );
-    
+
+    parameter SIZE = 1;
+
     reg [SIZE-1:0] i;
     reg [SIZE-1:0] to_shift;
-    reg [SIZE-1:0] temp;
+    wire [SIZE-1:0] temp;
     wire [2*SIZE-1:0] extended_mantissa;
-    
+    wire [2**SIZE-1:0] allZeros;
+
     always @(mantissa_in) begin
         to_shift = 0;
         for (i = SIZE; i >=0; i=i-1) begin
@@ -19,13 +22,13 @@ module NormalizeReg #(SIZE=1) (
             end
         end
     end
-    
+
     assign extended_mantissa = {mantissa_in,{SIZE{1'b0}}};
-    
+
     assign mantissa_out = extended_mantissa[to_shift -: 2**SIZE];
-    
+
     TwosComplement #(.SIZE(SIZE)) comp(.in(to_shift), .out(temp));
-    
+
     VariableCLA #(.SIZE(SIZE)) cla(.a(2**SIZE), .b(temp), .c_in(1'b0), .s(shiftNum));
-    
+
 endmodule
--- FinalProject.srcs/sources_1/new/TwosComplement.v
+++ FinalProject.srcs/sources_1/new/TwosComplement.v
@@ -1,16 +1,21 @@
 `timescale 1ns / 1ps
 
-module TwosComplement #(SIZE=0)(
-    input [2**SIZE:0] in,
-    output [2**SIZE:0] out
+module TwosComplement (
+    input [2**SIZE-1:0] in,
+    output [2**SIZE-1:0] out
     );
-    
-    wire [2**SIZE:0] in_inv;
-    
-    for (genvar i = 0; i < 2**SIZE; i=i+1) begin
+
+    parameter SIZE = 0;
+
+    wire [2**SIZE - 1:0] in_inv;
+    wire [2**SIZE - 1:0] bVal;
+    assign bVal = 1'b1;
+
+    genvar i;
+    for (i = 0; i < 2**SIZE; i = i+1) begin
         assign in_inv[i] = !in[i];
     end
-    
-    VariableCLA #(.SIZE(SIZE)) adder (.a(in_inv), .b(31'd1), .c_in(0), .s(out));
-    
+
+    VariableCLA #(.SIZE(SIZE)) adder (.a(in_inv), .b(bVal), .c_in(1'b0), .s(out));
+
 endmodule
