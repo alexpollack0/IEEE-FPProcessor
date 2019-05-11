536f1cbc695b45989df8dfec7288e655f5e5ada5
Alex Pollack <azpollack97@gmail.com>
2019-05-11 19:24:38 -0400

    Adder handling denormals, NaNs, inf

--- FinalProject.srcs/sources_1/new/FPAdder.v
+++ FinalProject.srcs/sources_1/new/FPAdder.v
@@ -31,6 +31,7 @@ module FPAdder(reset, clk, a, b, op, outSign, outExp, outFract);
     reg [23:0] regS;
     wire [23:0] regSTwosComp;
     reg [23:0] regFiveS;
+    wire [23:0] finalRes;
     wire [23:0] normalizedFiveS;
     wire [4:0] fiveBitShift;
     wire fiveCOut;
@@ -47,29 +48,29 @@ module FPAdder(reset, clk, a, b, op, outSign, outExp, outFract);
     
     wire [31:0] incOutExp;
     wire cTrash;
+    wire [4:0] normTrash;
 
     localparam ADD = 1'b0, SUB = 1'b1;
 
-    localparam INIT = 3'b000, SWAP_COMP = 3'b001, SHIFTB = 3'b010, NORM = 3'b011,
+    localparam INIT = 3'b000, SWAP = 3'b001, SHIFTB = 3'b010, NORM = 3'b011,
                 ADD_SHIFT = 3'b100, ADJUST =3'b101, ROUND_COMP = 3'b110, DONE = 3'b111;
 
     TwosComplement #(.BIT_WIDTH(24)) twoB(.in(bFract), .out(bFractComp));
     TwosComplement #(.BIT_WIDTH(25)) compShift(.in({1'b0, bFract}), .out(compShiftRegB));
-    TwosComplement #(.BIT_WIDTH(48)) compShiftOut(.in({22'b0, shiftedOut}), .out(compShiftedOut));
-    
-    VariableCLA #(.SIZE(5)) fourS(.a({8'b0, shiftRegB}), .b({8'b0, aFract}), .c_in(1'b0), .s(regSTemp), .c_out(fiveCOut));
-    assign fiveCOut = regSTemp[24];
-    
-    
-    TwosComplement #(.BIT_WIDTH(24)) fourComp(.in(regSTemp[23:0]), .out(regSTwosComp));
-    
-    NormalizeReg #(.BIT_WIDTH(24))fiveNormalize(.mantissa_in(regS), .mantissa_out(normalizedFiveS), .shiftNum(fiveBitShift));
-    
-    VariableCLA #(.SIZE(5)) sevenRound(.a({8'b0, regFiveS}), .b({31'b0, 1'b1}), .c_in(1'b0), .s(roundedVal), .c_out(roundCOut));
-    
+    TwosComplement #(.BIT_WIDTH(48)) compShiftOut(.in({22'b0, shiftedOut}), .out(compShiftedOut));    
+    VariableCLA #(.SIZE(5)) fourS(.a({8'b0, shiftRegB}), .b({8'b0, aFract}), .c_in(1'b0), .s(regSTemp), .c_out(fiveCOut));    
+    TwosComplement #(.BIT_WIDTH(24)) fourComp(.in(regSTemp[23:0]), .out(regSTwosComp));    
+    NormalizeReg #(.BIT_WIDTH(24))fiveNormalize(.mantissa_in(regS), .mantissa_out(normalizedFiveS), .shiftNum(fiveBitShift));    
+    VariableCLA #(.SIZE(5)) sevenRound(.a({8'b0, regFiveS}), .b({31'b0, 1'b1}), .c_in(1'b0), .s(roundedVal), .c_out(roundCOut));    
     VariableCLA #(.SIZE(5)) incOne(.a({24'b0, outExp}), .b({31'b0, 1'b1}), .c_in(1'b0), .s(incOutExp), .c_out(cTrash));
     VariableCLA #(.SIZE(3)) expDiff(.a(aExp), .b(~bExp), .c_in(1'b1), .s(d), .c_out(cTrash));
-    VariableCLA #(.SIZE(3)) subExp(.a(outExp), .b(~{3'b0,fiveBitShift}), .c_in(1'b1), .s(subOutExp), .c_out(cTrash));
+    VariableCLA #(.SIZE(3)) subExp(.a(outExp), .b(~{3'b0,fiveBitShift}), .c_in(1'b1), .s(subOutExp), .c_out(cTrash));    
+    NormalizeReg #(.BIT_WIDTH(24))finalNorm(.mantissa_in(regFiveS), .mantissa_out(finalRes), .shiftNum(normTrash));
+    
+    assign fiveCOut = regSTemp[24];
+    
+    reg aPosInf, aNegInf, bPosInf, bNegInf;
+    reg [4:0] inf;
     
     always @(negedge clk or negedge reset) begin
 
@@ -78,7 +79,6 @@ module FPAdder(reset, clk, a, b, op, outSign, outExp, outFract);
       end
       else begin
         state <= next_state;
-        #1 $display("SETTING STATE: %d", state);
       end
 
     end
@@ -90,34 +90,241 @@ module FPAdder(reset, clk, a, b, op, outSign, outExp, outFract);
         case(state)
 
             // Initialize the registers holding the sign, exponent, and fractional values
-            // Step 0
             INIT: begin
 
-              aSign <= a[31];
-              aExp <= a[30:23];
-              bExp <= b[30:23];
-              aFract <= {1'b1, a[22:0]};
-              bFract <= {1'b1, b[22:0]};
-              next_state <= SWAP_COMP;
-
-              outExp <= 0;
-              setComp <= 0;
-              swapped <= 0;
-
-              // TODO: Must take twos complement if subtraction
-              if(op) begin
-                  bSign = ~b[31];
+              // NaN
+              if((a[30:23] == 8'hff && a[22:0] != 0) || (b[30:23] == 8'hff && b[22:0] != 0)) begin
+                outExp = 8'hff;
+                outFract = 23'b1;
+                outSign = 1'b0;
+                next_state = DONE;
+              end
+              // Inf
+              else if((a[30:23] == 8'hff && a[22:0] == 23'b0)
+                        || (b[30:23] == 8'hff && b[22:0] == 23'b0)) begin
+                        
+                if(a[30:23] == 8'hff && a[22:0] == 23'b0) begin
+                    case(a[31])
+                        // a is positive infinity
+                        1'b0: begin
+                            aPosInf = 1; 
+                            aNegInf = 0;
+                        end
+                        // a is negative infinity
+                        1'b1: begin
+                            aNegInf = 1; 
+                            aPosInf = 0;
+                        end
+                    endcase
+                end
+                else begin
+                    aPosInf = 0;
+                    aNegInf = 0;
+                end
+                if(b[30:23] == 8'hff && b[22:0] == 23'b0) begin
+                    case(b[31])
+                        // a is positive infinity
+                        1'b0: begin
+                            bPosInf = 1; 
+                            bNegInf = 0;
+                        end
+                        // a is negative infinity
+                        1'b1: begin
+                            bNegInf = 1; 
+                            bPosInf = 0;
+                        end    
+                    endcase
+                end
+                else begin
+                    bPosInf = 0;
+                    bNegInf = 0;
+                end
+                
+                inf = {aPosInf, aNegInf, bPosInf, bNegInf, op};
+                case(inf)
+                    5'b00000: begin
+                        // This should never happen
+                    end
+                    // a + negInf = negInf
+                    5'b00010: begin
+                        outExp = 8'hff;
+                        outFract = 23'b0;
+                        outSign = 1'b1;
+                    end
+                    // a + posInf = posInf
+                    5'b00100: begin
+                        outExp = 8'hff;
+                        outFract = 23'b0;
+                        outSign = 1'b0;
+                    end
+                    5'b00110: begin
+                        // This should never hapen
+                    end
+                    // negInf + b = negInf
+                    5'b01000: begin
+                        outExp = 8'hff;
+                        outFract = 23'b0;
+                        outSign = 1'b1;
+                    end
+                    // negInf + negInf = negInf
+                    5'b01010: begin
+                        outExp = 8'hff;
+                        outFract = 23'b0;
+                        outSign = 1'b1;                
+                    end
+                    // nefInf + posInf = NaN
+                    5'b01100: begin
+                        outExp = 8'hff;
+                        outFract = 23'b1;
+                        outSign = 1'b0;                    
+                    end
+                    5'b01110: begin
+                        // This should never happen
+                    end
+                    // posInf + b = posInf
+                    5'b10000: begin
+                        outExp = 8'hff;
+                        outFract = 23'b0;
+                        outSign = 1'b0;
+                    end
+                    // posInf + negInf = NaN
+                    5'b10010: begin
+                        outExp = 8'hff;
+                        outFract = 23'b1;
+                        outSign = 1'b0;                    
+                    end
+                    // posInf + posInf = posInf
+                    5'b10100: begin
+                        outExp = 8'hff;
+                        outFract = 23'b0;
+                        outSign = 1'b0;                    
+                    end
+                    5'b10110: begin
+                        // This should never happen
+                    end
+                    5'b11000: begin
+                        // This should never happen
+                    end
+                    5'b11010: begin
+                        // This should never happen
+                    end
+                    5'b11100: begin
+                        // This should never happen
+                    end
+                    5'b11110: begin
+                       // This should never happen
+                    end
+                    5'b00001: begin
+                        // This should never happen
+                    end
+                    // a - negInf = posInf
+                    5'b00011: begin
+                        outExp = 8'hff;
+                        outFract = 23'b0;
+                        outSign = 1'b0;     
+                    end
+                    // a - posInf = negInf
+                    5'b00101: begin
+                        outExp = 8'hff;
+                        outFract = 23'b0;
+                        outSign = 1'b1;
+                    end
+                    5'b00111: begin
+                        // This should never happen
+                    end
+                    // negInf - b = negInf
+                    5'b01001: begin
+                        outExp = 8'hff;
+                        outFract = 23'b0;
+                        outSign = 1'b1;
+                    end
+                    // negInf - negInf  = NaN
+                    5'b01011: begin
+                        outExp = 8'hff;
+                        outFract = 23'b1;
+                        outSign = 1'b0; 
+                    end
+                    // negInf - posInf = negInf
+                    5'b01101: begin
+                        outExp = 8'hff;
+                        outFract = 23'b0;
+                        outSign = 1'b1;
+                    end
+                    5'b01111: begin
+                        // This should never happen
+                    end
+                    // posInf - b = posInf
+                    5'b10001: begin
+                        outExp = 8'hff;
+                        outFract = 23'b0;
+                        outSign = 1'b0;     
+                    end
+                    // posInf - negInf = posInf
+                    5'b10011: begin
+                        outExp = 8'hff;
+                        outFract = 23'b0;
+                        outSign = 1'b0;     
+                    end
+                    // posInf - posInf = NaN
+                    5'b10101: begin
+                        outExp = 8'hff;
+                        outFract = 23'b1;
+                        outSign = 1'b0; 
+                    end
+                    5'b10111: begin
+                        // This should never happen
+                    end
+                    5'b11001: begin
+                        // This should never happen                    
+                    end
+                    5'b11011: begin
+                        // This should never happen                   
+                    end
+                    5'b11101: begin
+                        // This should never happen                    
+                    end
+                    5'b11111: begin
+                        // This should never happen                    
+                    end                    
+                endcase
+                
+                next_state = DONE;
               end
               else begin
-                  bSign = b[31];
+                  aSign = a[31];
+                  aExp = a[30:23];
+                  bExp = b[30:23];
+                  if(aExp > 0) begin
+                    aFract = {1'b1, a[22:0]};
+                  end
+                  else begin
+                     aFract = {1'b0, a[22:0]};
+                  end
+                  if(bExp > 0) begin
+                     bFract = {1'b1, b[22:0]};
+                  end
+                  else begin
+                    bFract = {1'b0, b[22:0]};
+                  end
+                  next_state = SWAP;
+    
+                  outExp = 0;
+                  setComp = 0;
+                  swapped = 0;
+    
+                  // TODO: Must take twos complement if subtraction
+                  if(op) begin
+                      bSign = ~b[31];
+                  end
+                  else begin
+                      bSign = b[31];
+                  end
+                  next_state <= ADJUST;
               end
 
             end
 
-            // If aExp < bExp, swap the operands
-            // If the signs of the two exponents differ, replace b's fraction by its twos complement
-            // Steps 1, 2
-            SWAP_COMP: begin
+            SWAP: begin
              
               if(aExp < bExp) begin
 
@@ -146,19 +353,11 @@ module FPAdder(reset, clk, a, b, op, outSign, outExp, outFract);
               next_state <= SHIFTB;
             end
 
-            // Step 3
             SHIFTB: begin
-              
-              
-              $display("Shifting b by %d", d);
-              
+            
               if(aSign != bSign) begin
-                $display("compShiftRegB before shifting: %b", compShiftRegB);
                 shiftRegB = compShiftRegB >>> d;
                 shiftedOut = {compShiftRegB, 24'b0};
-                
-                $display("shiftRegB: %b", shiftRegB);
-                $display("shiftedOut: %b", shiftedOut);
               end
               else begin
                 shiftRegB = bFract >>> d;
@@ -166,82 +365,63 @@ module FPAdder(reset, clk, a, b, op, outSign, outExp, outFract);
               end
               
               shiftedOut = shiftedOut >>> d;
-              $display("After shifting shiftedOut: %b", shiftedOut);
 
               next_state = ADD_SHIFT;
 
             end
 
-            // Steps 4, 5
             ADD_SHIFT: begin
 
-              $display("Inside ADD_SHIFT");
-              // Finish initialization from Step 3
               next_state <= NORM;
               
               guard = shiftedOut[23];
               round = shiftedOut[22];
               sticky = |shiftedOut[21:0];
 
-              // Step 4
-
-                
-              $display("regSTemp: %b", regSTemp);
-
               // Add aFract to shiftRegB (call output register regS)
               regS = regSTemp[23:0];
-
-              $display("regS: %b", regS);
 
               // This can only possibly be hit when totalShift = 0
               if(aSign != bSign && regS[22] && !regSTemp[24]) begin
                 regS = regSTwosComp;
-                $display("Replaced regS with regSTwosComp");
                 bSign = aSign;
                 setComp = 1;
               end
 
-              $display("Carry out: %b", fiveCOut);
               
 
             end
             
             NORM: begin
             
-              // Step 5
               if(aSign == bSign && regSTemp[24]) begin
-                $display("Carry out, shifting in 1");
                 regFiveS = {1'b1, regS[23:1]};
-                $display("regFiveS: %b", regFiveS);
                 fiveLeft = 0;
               end
               else begin
-                $display("No carry out, using normalized version - %b", normalizedFiveS);
-                $display("Guard: %b", guard);
-                $display("Shifted left by %d", fiveBitShift);
-                regFiveS = normalizedFiveS;
-                regFiveS[fiveBitShift-1] = guard;
-                fiveLeft = 1;
+                if(d == 0) begin
+                    regFiveS = regS;
+                end
+                else begin
+                    regFiveS = normalizedFiveS;
+                    regFiveS[fiveBitShift-1] = guard;
+                    fiveLeft = 1;
+                end
               end
 
               // Adjust the exponent of the result
               if(fiveLeft) begin
                 outExp = subOutExp;
               end
-              else begin
+              else if(aSign == bSign && regSTemp[24]) begin
                 outExp = incOutExp[7:0];
-                $display("outExp incrementing by 1 - %b", outExp);
               end
-
-              $display("Next state set to ADJUST");
-              next_state <= ADJUST;
+              
             
             end
 
-            // Step 6
             ADJUST: begin
 
-              $display("Inside ADJUST");
               if(fiveLeft) begin
                 if(fiveBitShift > 1) begin
                   round = 0;
@@ -261,19 +441,13 @@ module FPAdder(reset, clk, a, b, op, outSign, outExp, outFract);
 
             end
 
-            // Steps 7, 8
             ROUND_COMP: begin
 
-              $display("Inside ROUND_COMP");
-              $display("Output fract value pre round: %b", regFiveS);
-              // Step 7: Round to nearest even
               if((round && regFiveS[23]) || (round && sticky)) begin
-                $display("Rounding baby!");
                 // Add 1
                 regFiveS <= roundedVal[23:0];
               end
 
-              // Step 8
               if(aSign != bSign) begin
                 eightCase = {swapped, setComp, aSign, bSign};
 
@@ -297,8 +471,12 @@ module FPAdder(reset, clk, a, b, op, outSign, outExp, outFract);
             end
 
             DONE: begin
-              $display("Inside DONE");
-              outFract <= regFiveS[22:0];
+              if(outExp != 0 && outExp != 8'hff) begin
+                 outFract = finalRes[22:0];
+              end
+              else if (outExp != 8'hff) begin
+                 outFract = regFiveS[22:0];
+              end
             end
 
         endcase
